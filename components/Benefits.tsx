996da53ca6a19d03f9cb0d73e216cb56b7cb8550
Maika <contacto.maikua@gmail.com>
2025-07-09 23:40:04 -0600

    Arreglar columnas

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -41,7 +41,7 @@ export default function Benefits() {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {benefits.map((benefit, index) => {
             const Icon = benefit.icon;
             return (
